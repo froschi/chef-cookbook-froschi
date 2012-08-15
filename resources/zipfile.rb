58055a459f0fb5733c54916ae0fcd184a76777ab
Thorsten Fischer <thorsten@froschi.org>
2012-08-15 12:54:32 +0200

    Added zipfile resource.
    Blatantly pulled from Opscode's Windows cookbook. To be used
    for system-wide installations of Firefox add-ons; the .xpi
    files are actually zipfiles.
    
    Provider following shortly.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

---
+++ resources/zipfile.rb
@@ -0,0 +1,10 @@
+actions :unzip
+
+attribute :path, :kind_of => String, :name_attribute => true
+attribute :source, :kind_of => String
+attribute :overwrite, :kind_of => [ TrueClass, FalseClass ], :default => false
+
+def initialize(name, run_context = nul)
+  super
+  @action = :unzip
+end
