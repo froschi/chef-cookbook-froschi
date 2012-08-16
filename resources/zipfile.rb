392db10cdf99dce5814b10aa2b581ed5bda8c28f
Thorsten Fischer <thorsten@froschi.org>
2012-08-16 23:29:29 +0200

    Fixed typo in resources file.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- resources/zipfile.rb
+++ resources/zipfile.rb
@@ -4,7 +4,7 @@ attribute :path, :kind_of => String, :name_attribute => true
 attribute :source, :kind_of => String
 attribute :overwrite, :kind_of => [ TrueClass, FalseClass ], :default => false
 
-def initialize(name, run_context = nul)
+def initialize(name, run_context = nil)
   super
   @action = :unzip
 end
