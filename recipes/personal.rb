d6d941f4a0b88aa8c875e0830149334f2f170194
Thorsten Fischer <thorsten.fischer@zalando.de>
2012-09-17 10:58:53 +0200

    Play with deploy resource in personal recipe.
    Assumes a rails structure. Doesn't really to much for me in the
    case of vim plugin deployment. Will probably revert to using
    remote files.
    
    Signed-off-by: Thorsten Fischer <thorsten.fischer@zalando.de>

--- recipes/personal.rb
+++ recipes/personal.rb
@@ -36,8 +36,26 @@ directory vimdir do
   mode 0755
 end
 
-git "#{homedir}/.csolo/pathogen/" do
+git "#{homedir}/.csolo/pathogen" do
   repository "https://github.com/tpope/vim-pathogen.git"
   reference 'v2.0'
   action :sync
 end
+
+# The following works as intended; however, what's intended is not
+# what I really need.
+#directory "#{homedir}/.vim/testing/shared" do
+#  owner user
+#  group group
+#  recursive true
+#  mode 0755
+#end
+#
+#deploy "#{homedir}/.vim/testing" do
+#  repo "#{homedir}/.csolo/pathogen"
+#  revision 'v2.0'
+#  create_dirs_before_symlink []
+#  purge_before_symlink []
+#  symlinks({})
+#  symlink_before_migrate({})
+#end
